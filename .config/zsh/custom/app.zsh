313e5a00e141822f85ef7e9de45b148f5f4498ce
Li Haotian <2878348186@qq.com>
2023-10-31 00:12:56 +0800

    change conda path

--- .config/zsh/custom/app.zsh
+++ .config/zsh/custom/app.zsh
@@ -41,17 +41,19 @@ bindkey ^q _sgpt_zsh
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/usr/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/lilin/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/usr/etc/profile.d/conda.sh" ]; then
-        . "/usr/etc/profile.d/conda.sh"
+    if [ -f "/home/lilin/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/lilin/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/usr/bin:$PATH"
+        export PATH="/home/lilin/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
+

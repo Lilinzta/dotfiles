caf7294b38ef69ebf8e71208d715b350a2027284
Li Haotian <2878348186@qq.com>
2023-10-31 12:37:51 +0800

    change path

--- .config/zsh/custom/app.zsh
+++ .config/zsh/custom/app.zsh
@@ -44,14 +44,14 @@ bindkey ^q _sgpt_zsh
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/lilin/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/lilin/.conda/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/lilin/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/lilin/miniconda3/etc/profile.d/conda.sh"
+    if [ -f "/home/lilin/.conda/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/lilin/.conda/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/home/lilin/miniconda3/bin:$PATH"
+        export PATH="/home/lilin/.conda/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup

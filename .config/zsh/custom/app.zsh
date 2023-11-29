fe7759dde2ea5c8a2b439936adbb86e1e2f97938
Li Haotian <2878348186@qq.com>
2023-11-29 15:32:51 +0800

    change " 2 '

--- .config/zsh/custom/app.zsh
+++ .config/zsh/custom/app.zsh
@@ -15,7 +15,7 @@ function nvs() {
   items=("default" "ayamir" "LazyVim" "NvChad")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config ❯ " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
-    echo "Nothing selected"
+    echo 'Nothing selected'
     return 0
   elif [[ $config == "default" ]]; then
     config=""

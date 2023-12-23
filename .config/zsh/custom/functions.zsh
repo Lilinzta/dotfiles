5466d7c40d5b11d80c447039524d3731dd898d92
Haotian Li <lilinzta@gmail.com>
2023-12-23 12:31:43 +0800

    add joshuto auto change dir fn

--- .config/zsh/custom/functions.zsh
+++ .config/zsh/custom/functions.zsh
@@ -7,6 +7,30 @@ function ra() {
     fi
     rm -f -- "$temp_file"
 }
+function jo() {
+	ID="$$"
+	mkdir -p /tmp/$USER
+	OUTPUT_FILE="/tmp/$USER/joshuto-cwd-$ID"
+	env joshuto --output-file "$OUTPUT_FILE" $@
+	exit_code=$?
+
+	case "$exit_code" in
+		# regular exit
+		0)
+			;;
+		# output contains current directory
+		101)
+			JOSHUTO_CWD=$(cat "$OUTPUT_FILE")
+			cd "$JOSHUTO_CWD"
+			;;
+		# output selected files
+		102)
+			;;
+		*)
+			echo "Exit code: $exit_code"
+			;;
+	esac
+}
 function bl() {
   echo `expr $1 \* 75` | sudo tee /sys/class/backlight/intel_backlight/brightness
 }

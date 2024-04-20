8c73dd43cb77d2f8673c089c60604e8b00cc4a4c
Haotian Li <lilinzta@gmail.com>
2024-04-20 15:55:52 +0800

    add mambaforge and rm p10k

--- .config/zsh/custom/app.zsh
+++ .config/zsh/custom/app.zsh
@@ -1,12 +1,5 @@
 # Ok, This is automatically added by some ptograms.
 
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
 # neovim switcher
 alias lvim="NVIM_APPNAME=LazyVim nvim"
 alias vimir="NVIM_APPNAME=ayamir nvim"
@@ -38,8 +31,5 @@ zle -N _sgpt_zsh
 bindkey ^q _sgpt_zsh
 # Shell-GPT integration ZSH v0.1
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-# miniconda3
-[ -f /opt/miniconda3/etc/profile.d/conda.sh ] && source /opt/miniconda3/etc/profile.d/conda.sh
+# Activate Mambaforge
+[ -f /opt/mambaforge/etc/profile.d/conda.sh ] && source /opt/mambaforge/etc/profile.d/conda.sh

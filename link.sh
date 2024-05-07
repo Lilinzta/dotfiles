e8634645dba3a39fb6386983bde127a3ee596b5b
Haotian Li <lilinzta@gmail.com>
2024-05-07 13:42:37 +0800

    add bat btop fastfetch lsd config

--- link.sh
+++ link.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-dotconf=$HOME/git/dotfiles/.config
 dot=$HOME/git/dotfiles
+dotconf=$dot/.config
+conf=$HOME/.config
 
+# Create symbolic links for dotfiles in the home directory
 ln -s $dot/.tmux.conf $HOME/.tmux.conf
 ln -s $dot/.ideavimrc $HOME/.ideavimrc
 ln -s $dot/.gitconfig $HOME/.gitconfig
@@ -10,13 +12,18 @@ ln -s $dot/.vimrc $HOME/.vimrc
 ln -s $dot/.zshrc $HOME/.zshrc
 ln -s $dot/.vim $HOME/.vim
 
-ln -s $dotconf/fontconfig $HOME/.config/fontconfig
-ln -s $dotconf/alacritty $HOME/.config/alacritty
-ln -s $dotconf/neofetch $HOME/.config/neofetch
-ln -s $dotconf/joshuto $HOME/.config/joshuto
-ln -s $dotconf/ranger $HOME/.config/ranger
-ln -s $dotconf/kitty $HOME/.config/kitty
-ln -s $dotconf/helix $HOME/.config/helix
-ln -s $dotconf/nvim $HOME/.config/nvim
-ln -s $dotconf/mpv $HOME/.config/mpv
-ln -s $dotconf/zsh $HOME/.config/zsh
+# Create symbolic links for dotfiles in the .config directory
+ln -s $dotconf/fontconfig $conf/fontconfig
+ln -s $dotconf/alacritty $conf/alacritty
+ln -s $dotconf/fastfetch $conf/fastfetch
+ln -s $dotconf/neofetch $conf/neofetch
+ln -s $dotconf/joshuto $conf/joshuto
+ln -s $dotconf/ranger $conf/ranger
+ln -s $dotconf/kitty $conf/kitty
+ln -s $dotconf/helix $conf/helix
+ln -s $dotconf/nvim $conf/nvim
+ln -s $dotconf/btop $conf/btop
+ln -s $dotconf/mpv $conf/mpv
+ln -s $dotconf/zsh $conf/zsh
+ln -s $dotconf/bat $conf/bat
+ln -s $dotconf/lsd $conf/lsd

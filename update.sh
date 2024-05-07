ad6a11e84d26f35997e80dabfd5943ea342e4eb2
Haotian Li <lilinzta@gmail.com>
2024-05-07 11:08:45 +0800

    reinit

--- update.sh
+++ update.sh
@@ -1,15 +1,22 @@
-cp ~/.zshrc .
-cp ~/.tmux.conf .
-cp ~/.vimrc .
-cp ~/.ideavimrc .
-cp ~/.gitconfig .
-cp -r ~/.config/alacritty ./.config
-cp -r ~/.config/mpv ./.config
-cp -r ~/.config/neofetch ./.config
-cp -r ~/.config/nvim ./.config
-cp -r ~/.config/ranger ./.config
-cp -r ~/.config/zsh ./.config
-cp -r ~/.config/kitty ./.config
-# cp -r ~/.tmux ./.config
-cp -r ~/.vim/autoload ./.vim
-pacman -Q >./pkglist.txt
+#!/bin/bash
+
+dotconf=$HOME/git/dotfiles/.config
+dot=$HOME/git/dotfiles
+
+ln -s $dot/.tmux.conf $HOME/.tmux.conf
+ln -s $dot/.ideavimrc $HOME/.ideavimrc
+ln -s $dot/.gitconfig $HOME/.gitconfig
+ln -s $dot/.vimrc $HOME/.vimrc
+ln -s $dot/.zshrc $HOME/.zshrc
+ln -s $dot/.vim $HOME/.vim
+
+ln -s $dotconf/fontconfig $HOME/.config/fontconfig
+ln -s $dotconf/alacritty $HOME/.config/alacritty
+ln -s $dotconf/neofetch $HOME/.config/neofetch
+ln -s $dotconf/joshuto $HOME/.config/joshuto
+ln -s $dotconf/ranger $HOME/.config/ranger
+ln -s $dotconf/kitty $HOME/.config/kitty
+ln -s $dotconf/helix $HOME/.config/helix
+ln -s $dotconf/nvim $HOME/.config/nvim
+ln -s $dotconf/mpv $HOME/.config/mpv
+ln -s $dotconf/zsh $HOME/.config/zsh

c991eadd4096ddd0677f303a7a2095d46b5b7e8c
Haotian Li <lilinzta@gmail.com>
2024-05-08 16:37:13 +0800

    add yaml python lsp

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,11 +3,14 @@ return {
   opts = {
     ensure_installed = {
       "bash-language-server",
+      "yaml-language-server",
       "lua-language-server",
+      "python-lsp-server",
       "rust-analyzer",
       "clang-format",
       "eslint-lsp",
       "prettier",
+      "yamlfmt",
       "clangd",
       "stylua",
       "shfmt",

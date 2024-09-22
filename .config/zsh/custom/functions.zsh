3e33859950cf2c8a21061d1d3c2efe1e05d8f014
Haotian Li <lilinzta@gmail.com>
2024-09-22 08:54:45 +0800

    rm function word and add sgpt fy

--- .config/zsh/custom/functions.zsh
+++ .config/zsh/custom/functions.zsh
@@ -1,5 +1,5 @@
 # Auto change dir---yazi
-function ya() {
+ya() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
@@ -9,7 +9,7 @@ function ya() {
 }
 
 # Auto change dir---ranger
-function ra() {
+ra() {
     temp_file="$(mktemp -t "ranger_cd.XXXXXXXXXX")"
     ranger --choosedir="$temp_file" -- "${@:-$PWD}"
     if chosen_dir="$(cat -- "$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
@@ -19,7 +19,7 @@ function ra() {
 }
 
 # Auto change dir---joshuto
-function jo() {
+jo() {
 	ID="$$"
 	mkdir -p /tmp/$USER
 	OUTPUT_FILE="/tmp/$USER/joshuto-cwd-$ID"
@@ -44,16 +44,20 @@ function jo() {
 	esac
 }
 
-function ag() {
+ag() {
   cd /home/lilin/code/python/auto_giwifi_python_v2/
   ./venv/bin/python main.py
 }
 
-function bl() {
+bl() {
   echo `expr $1 \* 75` | sudo tee /sys/class/backlight/intel_backlight/brightness
 }
 
-function mcd() {
+fy () {
+  sgpt --role Translator $1
+}
+
+mcd() {
     mkdir -p "$1"
     cd "$1"
 }

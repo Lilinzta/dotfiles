4867a22fabf454c943d13d3a4f94db75f106ddf0
Haotian Li <lilinzta@gmail.com>
2024-10-09 21:56:15 +0800

    let atuin manage alias

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -2,7 +2,6 @@ export ZP=$HOME/.config/zsh
 source $ZP/zinitaller.zsh
 source $ZP/custom/functions.zsh
 source $ZP/custom/plugins.zsh
-source $ZP/custom/alias.zsh
 source $ZP/custom/app.zsh
 source $ZP/custom/env.zsh
 source $ZP/default.zsh
